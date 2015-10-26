f614c0d772cfd1b2584bbf5169519a7979163a7d
london <londonxue@163.com>
2015-10-26 09:30:15 +0800

    Initial Commit

---
+++ foo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  foo
+//
+//  Created by london xue on 26/10/15.
+//  Copyright (c) 2015 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ foo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  foo
+//
+//  Created by london xue on 26/10/15.
+//  Copyright (c) 2015 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
